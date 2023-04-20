acc50bb053a535cf43763747af23cc1d9062900f
ebeling <ebeling@physik.uni-kiel.de>
2023-04-20 10:01:05 +0200

    Greetings neu

--- Vorlesung_3/Greetings.java
+++ Vorlesung_3/Greetings.java
@@ -1,8 +1,8 @@
 public class Greetings {
-5
+
     public static void main(String[] args) {
 
-        System.out.println('Hello World!')
+        System.out.println("Hello World!");
         
     }
 
