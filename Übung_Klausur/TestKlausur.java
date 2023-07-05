fc196b47bed22602db40354d53d43479bbe6fb3f
ebeling <ebeling@physik.uni-kiel.de>
2023-07-05 11:23:28 +0200

    Übung Klausur

---
+++ Übung_Klausur/TestKlausur.java
@@ -0,0 +1,9 @@
+public class TestKlausur {
+    public static void main(String args[]) {
+        int i = 0;
+        System.out.println(++i * 2.0);
+        System.out.println((double)(int)3.2*2);
+        i = 0.0;
+        System.out.println(i);
+    }
+}
